b0fe767b2e258f762597cb43f0634394b6c6b9ce
DimaG <dimagilev91@gmail.com>
2025-09-01 16:03:29 +0500

    mob

---
+++ myfirstgame/Scenes/Skeleton/skeleton.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var chase: bool = false
+var speed: int = 100
+
+func  _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	var player = $"../../Player"
+	var direction = (player.position - self.position).normalized()
+	if chase:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()
+
+func _on_detector_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = true
+
+
+func _on_detector_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		chase = false
