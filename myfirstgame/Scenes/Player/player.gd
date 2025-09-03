e4d2a2842ab6c3c7afb930091f22ea391528f1f0
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-03 21:41:11 +0500

    gold

--- myfirstgame/Scenes/Player/player.gd
+++ myfirstgame/Scenes/Player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var health: int = 100
+var gold: int = 0
 
 @onready var anim: AnimatedSprite2D = $Anim
 @onready var camera_2d: Camera2D = $Camera2D
