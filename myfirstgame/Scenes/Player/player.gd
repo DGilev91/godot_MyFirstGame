b0159a464702160c5141228f33cf093a3c116540
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-06 08:59:43 +0500

    a

--- myfirstgame/Scenes/Player/player.gd
+++ myfirstgame/Scenes/Player/player.gd
@@ -16,13 +16,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("attack") and is_on_floor():
 		anim.play("Jump")
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:	
 		if velocity.y == 0:	
 			anim.play("Run")
