fbe59382f1cedc64d13cd902380b5129639df9dc
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-11 11:21:18 +0500

    days counter

--- myfirstgame/Scenes/Level/level.gd
+++ myfirstgame/Scenes/Level/level.gd
@@ -8,24 +8,19 @@ enum  State {
 }
 
 var state: State = State.NIGHT
+var day_count: int = 1
+
 @onready var directional_light_2d: DirectionalLight2D = $DirectionalLight2D
 @onready var point_light_2d: PointLight2D = $PointLight2D
+@onready var day_text: Label = $CanvasLayer/DayText
+@onready var animation_player: AnimationPlayer = $CanvasLayer/AnimationPlayer
+
 
 
 func  _ready() -> void:
 	directional_light_2d.enabled = true
-
-func  _process(_delta: float) -> void:
-	match state:
-		State.MORNING:
-			morning_state()
-		State.DAY:
-			day_state()
-		State.EVENING:
-			evening_state()
-		State.NIGHT:
-			night_state()
-			
+	set_dat_text()		
+		
 func morning_state():
 	var tween = get_tree().create_tween()
 	tween.tween_property(directional_light_2d, "energy", 0.5, 3)
@@ -54,7 +49,19 @@ func night_state():
 	var tween1 = get_tree().create_tween()
 	tween1.tween_property(point_light_2d, "energy", 3.5, 3)
 
-func _on_day_night_timeout() -> void:
+func _on_day_night_timeout() -> void:	
+	match state:
+		State.MORNING:
+			day_count += 1
+			set_dat_text()		
+			morning_state()
+		State.DAY:
+			day_state()
+		State.EVENING:
+			evening_state()
+		State.NIGHT:
+			night_state()
+	
 	match state:
 		State.MORNING:
 			state = State.DAY
@@ -64,3 +71,10 @@ func _on_day_night_timeout() -> void:
 			state = State.NIGHT
 		State.NIGHT:
 			state = State.MORNING
+
+func set_dat_text():
+	day_text.text = "Day " + str(day_count)
+	animation_player.play("day_text_fade_in")
+	await get_tree().create_timer(3).timeout
+	animation_player.play("day_text_fade_out")
+	
